397a6ef331a759d3706d6d54ca281a4cea4bd0a9
Hwanghyewon <asdd1103@naver.com>
2025-09-01 17:46:31 +0900

    portfolio-2025-09-01

---
+++ JS/hhw_main.js
@@ -0,0 +1,21 @@
+const visualImg = document.querySelector('.visual');
+const moonImg = document.querySelector('.visual .moon');
+const Description = document.querySelectorAll('.Description p')
+
+moonImg.addEventListener('mouseover', function () {
+  visualImg.classList.add('back-img');
+  Description.forEach(function(description) {
+    description.style.display = 'flex';
+  });
+});
+
+moonImg.addEventListener('mouseout', function () {
+  visualImg.classList.remove('back-img');
+  Description.forEach(function(description) {
+  description.style.display = 'none';
+  });
+});
+
+
+
+
